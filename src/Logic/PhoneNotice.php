e7381fc9ef648ed1d251df227c0a869469461d46
Sprovider90 <zhangjiulin@guxiansheng.cn>
2020-09-22 13:22:29 +0800

    '1'

--- src/Logic/PhoneNotice.php
+++ src/Logic/PhoneNotice.php
@@ -36,15 +36,15 @@ class PhoneNotice implements Icommand
                                 phonenotice
                         ) order by a.id asc limit 10;";
 
-            $sql = "SELECT
-                         a.*,b.id as projects_waring_setting_id,b.remind_time,b.percentage,b.notice_start_time,b.notice_end_time,notice_phone
-                    FROM
-                        `warnigs` a
-                    LEFT JOIN projects_waring_setting b ON a.project_id = b.project_id
-                    WHERE
-
-                        a.id =12174363 order by a.id asc limit 1;";
-            $rs = $db->getAll($sql);
+//            $sql = "SELECT
+//                         a.*,b.id as projects_waring_setting_id,b.remind_time,b.percentage,b.notice_start_time,b.notice_end_time,notice_phone
+//                    FROM
+//                        `warnigs` a
+//                    LEFT JOIN projects_waring_setting b ON a.project_id = b.project_id
+//                    WHERE
+//
+//                        a.id =12174363 order by a.id asc limit 1;";
+//            $rs = $db->getAll($sql);
 
             if(!empty($rs)){
                 foreach ($rs as $k=>$v){
@@ -62,7 +62,7 @@ class PhoneNotice implements Icommand
                     usleep(100);
                 }
             }
-            exit;
+//            exit;
         }
     }
     function saveToMysql($data)
