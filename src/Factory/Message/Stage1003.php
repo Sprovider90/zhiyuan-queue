57a72fa36583f5076b903a6812f9bf371a6b0a63
Sprovider90 <zhangjiulin@guxiansheng.cn>
2020-09-25 09:58:29 +0800

    '1'

--- src/Factory/Message/Stage1003.php
+++ src/Factory/Message/Stage1003.php
@@ -13,6 +13,7 @@ use Sprovider90\Zhiyuanqueue\Model\ZhiyuanData;
 class Stage1003 implements IMessageTrategy
 {
     protected $zhiyuandata;
+    protected $zhibaos_hash=["humidity"=>"湿度","temperature"=>"温度","formaldehydecd"=>"甲醛","PM25"=>"PM25","CO2"=>"CO2","TVOC"=>"TVOC"];
     public function __construct()
     {
         $this->zhiyuandata=new ZhiyuanData();
@@ -22,7 +23,16 @@ class Stage1003 implements IMessageTrategy
         $data["pro_name"]=$rs["pro_name"];
         $data["project_id"]=$rs["project_id"];
         $data["monitor_id"]=$rs["monitor_id"];
+        $data["target_values"]=$this->turn_target_values($data["target_values"]);
         return $data;
+    }
+    protected function turn_target_values($target_values){
+        $result=$target_values;
+        foreach ($this->zhibaos_hash as $k=>$v){
+            $result=str_replace($k,$v,$result);
+        }
+        print_r($result);exit;
+        return $result;
     }
     function getUsersByStage($data){
         return $this->zhiyuandata->getUsersFromWaring($data["warnig_id"]);
